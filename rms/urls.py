47ea503a11606334d6a4aa9cc6c2ac7a96eb3e27
CodeKnight <dearjinni44@gmail.com>
2026-06-21 10:41:12 +0545

    Implement generics (ListCreateAPIView, RetrieveUpdateDestroyAPIView

--- rms/urls.py
+++ rms/urls.py
@@ -5,26 +5,26 @@ urlpatterns = [
     
     # Category routes: 
     path('category/', CategoryView.as_view()),
-    path('category/<id>/', CategoryDetail.as_view()),
+    path('category/<pk>/', CategoryDetail.as_view()),
     
     # Food routes: 
     path('food/', FoodView.as_view()),
-    path('food/<id>/', FoodDetail.as_view()),
+    path('food/<pk>/', FoodDetail.as_view()),
     
     
     # Table routes: 
     path('table/', TableView.as_view()),
-    path('table/<id>/', TableDetail.as_view()),
+    path('table/<pk>/', TableDetail.as_view()),
     
     # Order routes: 
     path('order/', OrderView.as_view()),
-    path('order/<id>/', OrderDetail.as_view()),
+    path('order/<pk>/', OrderDetail.as_view()),
     
     # OrderItem routes: 
     path('order-item/', OrderItemView.as_view()),
-    path('order-item/<id>/', OrderItemDetail.as_view()),
+    path('order-item/<pk>/', OrderItemDetail.as_view()),
     
     # User routes:path('order/', OrderView.as_view()),
     path('user/', UserView.as_view()),
-    path('user/<id>/', UserDetail.as_view()),
+    path('user/<pk>/', UserDetail.as_view()),
 ]
--- rms/views.py
+++ rms/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework.generics import *
 
 from .models import *
 from .serializers import *
@@ -12,49 +14,17 @@ from .serializers import *
 
 # Category API ------------------------------------------------------------------
 
-## get, post:
-class CategoryView(APIView):
-    def get(self, request):
-        category = Category.objects.all()
-        serializer = CategorySerializer(category, many = True)  # serialization : object converted into json
-        
-        return Response(serializer.data, status.HTTP_200_OK)
+class CategoryView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
     
-    def post(self, request):
-        serializer = CategorySerializer(data = request.data)   # deserialization : json converted into object
-        serializer.is_valid(raise_exception = True)
-        serializer.save()
-        
-        return Response(serializer.data, status.HTTP_201_CREATED)
-
-
-
-## get, put, patch, delete:
-class CategoryDetail(APIView):
-    def get(self, request,id):
-        category = Category.objects.get(id=id)
-        serializer = CategorySerializer(category)
-        
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def put(self, request,id):
-        category = Category.objects.get(id=id)
-        serializer = CategorySerializer(category, data = request.data)
-        serializer.is_valid(raise_exception = True)
-        serializer.save()
-        
-        return Response(serializer.data, status.HTTP_201_CREATED)
-    
-    def patch(self, request,id):        # patch: partially updates the fields (all fields are not required) in the payload
-        category = Category.objects.get(id=id)
-        serializer = CategorySerializer(category, data = request.data, partial=True)
-        serializer.is_valid(raise_exception = True)
-        serializer.save()
-        
-        return Response(serializer.data, status.HTTP_201_CREATED)
-
-    def delete(self, request,id):
-        category = Category.objects.get(id = id)
+    def delete(self, request, *args, **kwargs):
+        category = self.get_object()
         item = OrderItem.objects.filter(food__category = category).count()
         if item >0:
             return Response("Protected: Category cannot be deleted. Related to Food in OrderItem.", 400)
@@ -66,266 +36,92 @@ class CategoryDetail(APIView):
 
 # Food API ----------------------------------------------------------------------
 
-## get, post:
-class FoodView(APIView):
-    def get(self, request):
-        food = Food.objects.all()
-        serializer = FoodSerializer(food, many = True)
-        
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def post(self, request):
-        serializer = FoodSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, status.HTTP_201_CREATED)
 
-
-## get, put, patch, delete: 
-class FoodDetail(APIView):
-    def get(self, request, id):
-        food = Food.objects.get(id = id)
-        serializer = FoodSerializer(food)
-        
-        return Response(serializer.data, 200)
+class FoodView(ListCreateAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
     
-    def put(self, request, id):
-        food = Food.objects.get(id=id)
-        serializer = FoodSerializer(food, data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
+class FoodDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
     
-    
-    def patch(self, request, id):
-        food = Food.objects.get(id=id)
-        serializer = FoodSerializer(food, data = request.data, partial = True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    
-    def delete(self, request, id):
-        food = Food.objects.get(id=id)
-        item = OrderItem.objects.filter(food = food ).count()
-        if item > 0:
-            return Response("Protected: Food cannot be deleted. Related to OrderItem.", 400)
+    def delete(self, request, *args, **kwargs):
+        food = self.get_object()
+        item = OrderItem.objects.filter(food = food).count()
+        if item >0:
+            return Response("Protected! Related to OrderItem.", 400)
         
         food.delete()
-        return Response("Deleted successfully", 204)
-    
-
+        return Response("Food deleted", 204)
+        
 
 # Table API --------------------------------------------------------
 
-## get,post: 
-class TableView(APIView):
-    def get(self, request):
-        food = Table.objects.all()
-        serializer = TableSerializer(food, many = True)
-        
-        return Response(serializer.data, status.HTTP_200_OK)
+class TableView(ListCreateAPIView):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
     
-    def post(self, request):
-        serializer = TableSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+class TableDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
+    
+    def delete(self, request, *args, **kwargs):
+        table = self.get_object()
+        item = OrderItem.objects.filter(order__table = table).count()
+        if item>0: 
+            return Response("Protected! Related to Order in OrderItem.", 400)
         
-        return Response(serializer.data, status.HTTP_201_CREATED)
+        table.delete()
+        return Response("Table deleted", 204)
 
 
-## get, put, patch, delete:
-
-class TableDetail(APIView):
-    def get(self, request, id):
-        food = Table.objects.get(id = id)
-        serializer = TableSerializer(food)
-        
-        return Response(serializer.data, 200)
-    
-    def put(self, request, id):
-        food = Table.objects.get(id=id)
-        serializer = TableSerializer(food, data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    def patch(self, request, id):
-        food = Table.objects.get(id=id)
-        serializer = TableSerializer(food, data = request.data, partial = True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    
-    def delete(self, request, id):
-        table = Table.objects.get(id=id)
-        item = OrderItem.objects.filter(order__table = table)
-        if item > 0:
-            return Response("Protected: Table cannot be delete. Related to Order in OrderItem.", 400)
-        
-        return Response("Deleted successfully", 204)
-    
-    
 
 # Order API -------------------------------------------------
 
-## get, post: 
-class OrderView(APIView):
-    def get(self, request):
-        order = Order.objects.all()
-        serializer = OrderSerializer(order, many = True)
-        
-        return Response(serializer.data, status.HTTP_200_OK)
+class OrderView(ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
     
-    def post(self, request):
-        serializer = OrderSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, status.HTTP_201_CREATED)
-
-
-## get, put, patch, delete: 
-class OrderDetail(APIView):
-    def get(self, request, id):
-        order = Order.objects.get(id = id)
-        serializer = OrderSerializer(order)
-        
-        return Response(serializer.data, 200)
+class OrderDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
     
-    def put(self, request, id):
-        order = Order.objects.get(id=id)
-        serializer = OrderSerializer(order, data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    def patch(self, request, id):
-        order = Order.objects.get(id=id)
-        serializer = OrderSerializer(order, data = request.data, partial = True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    
-    def delete(self, request, id):
-        order = Order.objects.get(id=id)
-        item = OrderItem.objects.filter(order = order).count()
+    def delete(self, request, *args, **kwargs):
+        order = self.get_object()
+        item = OrderItem.object.filter(order = order).count()
         if item > 0:
-            return Response("Protected: Order cannot be deleted. Related to OrderItem.", 400)
+            return Reponse("Protected!. Related to OrderItem.", 400)
         
         order.delete()
-        return Response("Deleted successfully", 204)
-    
-    
-    
+        return Response("Order deleted. ", 204)
+
+
 # OrderItem API -------------------------------------------------------
 
-## get, post: 
-class OrderItemView(APIView):
-    def get(self, request):
-        order_item = OrderItem.objects.all()
-        serializer = OrderItemSerializer(order_item, many = True)
-        
-        return Response(serializer.data, status.HTTP_200_OK)
+class OrderItemView(ListCreateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+class OrderItemDetail(RetrieveUpdateDestroyAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
     
-    def post(self, request):
-        serializer = OrderItemSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, status.HTTP_201_CREATED)
 
 
-## get, put, patch, delete: 
-class OrderItemDetail(APIView):
-    def get(self, request, id):
-        order_item = OrderItem.objects.get(id = id)
-        serializer = OrderItemSerializer(order_item)
-        
-        return Response(serializer.data, 200)
-    
-    def put(self, request, id):
-        order_item = OrderItem.objects.get(id=id)
-        serializer = OrderItemSerializer(order_item, data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    def patch(self, request, id):
-        order_item = OrderItem.objects.get(id=id)
-        serializer = OrderItemSerializer(order_item, data = request.data, partial = True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    
-    def delete(self, request, id):
-        OrderItem.objects.get(id=id).delete()
-        
-        return Response("Deleted successfully", 204)
+# User API ----------------------------------------------------------------
 
-
-
-# User API: ----------------------------------------
-
-## get, post: 
-class UserView(APIView):
-    def get(self, request):
-        user = User.objects.all()
-        serializer = UserSerializer(user, many = True)
-        
-        return Response(serializer.data)
+class UserView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     
-    def post(self, request):
-        serializer = UserSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data, 201)
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     
-    
-## get, put, patch, delete: 
-class UserDetail(APIView):
-    def get(self, request, id):
-        user = User.objects.get(id = id)
-        serializer = UserSerializer(user)
-    
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        user = User.objects.get(id = id )
-        serializer = UserSerializer(user, data= request.data)
-        serializer.is_valid()
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    def patch(self, request, id):
-        user = User.objects.get(id = id )
-        serializer = UserSerializer(user, data= request.data, partial = True)
-        serializer.is_valid()
-        serializer.save()
-        
-        return Response(serializer.data, 201)
-    
-    def delete(self, request, id):
-        user = User.objects.get(id = id )
+    def delete(self, request, *args, **kwargs):
+        user = self.get_object()
         item = OrderItem.objects.filter(order__user = user).count()
-        if item > 0:
-            return Response("Protected: Cannot be deleted. Related or Order in OrderItem.", 400)
+        if item > 0: 
+            return Respnse("Protected! Related to Order of OrderItem.", 400)
         
         user.delete()
-        return Response("User deleted successfully.", 204)
-    
-    
+        return Response("User deleted.", 204)
