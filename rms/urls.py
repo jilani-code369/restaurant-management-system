b4b3eb7772ea635c2f4c1b9a2621fada1f83ed58
CodeKnight <dearjinni44@gmail.com>
2026-06-23 10:17:17 +0545

    Implement 'routers' in urls.py

--- rms/urls.py
+++ rms/urls.py
@@ -1,34 +1,20 @@
 from .views import *
 from django.urls import path
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+
+router.register('category', CategoryAPI)
+router.register('food', FoodAPI)
+router.register('table', TableAPI)
+router.register('order', OrderAPI)
+router.register('order-item', OrderItemAPI)
+router.register('user', UserAPI)
+
+
+
 
 urlpatterns = [
     
-    # Category routes: 
-    path('category/', CategoryAPI.as_view({'get':'list', 'post' : 'create'})),
-    path('category/<pk>/', CategoryAPI.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'})),
-    
-    # Food routes: 
-    path('food/', FoodAPI.as_view({'get':'list', 'post':'create'})),
-    path('food/<pk>/', FoodAPI.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'})),
-    
-    
-    # Table routes: 
-    path('table/', TableAPI.as_view({'get':'list', 'post':'create'})),
-    path('table/<pk>/', TableAPI.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'})),
-    
-    
-    # Order routes: 
-    path('order/', OrderAPI.as_view({'get':'list', 'post':'create'})),
-    path('order/<pk>/', OrderAPI.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'})),
-    
-    
-    # OrderItem routes: 
-    path('order-item/', OrderItemAPI.as_view({'get':'list', 'post':'create'})),
-    path('order-item/<pk>/', OrderItemAPI.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'})),
-    
-    
-    # User routes:path('order/', OrderView.as_view()),
-    path('user/', UserAPI.as_view({'get':'list', 'post':'create'})),
-    path('user/<pk>/', UserAPI.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update', 'delete':'destroy'})),
-    
-]
+] + router.urls
